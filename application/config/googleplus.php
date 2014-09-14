01cb0ba8b145dd42fdb3f2073d1c86d4367e0422
rajesh ujade <rajeshujade@gmail.com>
2014-09-14 18:41:03 +0530

    google-plus-api-codeigniter-starter

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -1,9 +1,9 @@
 <?php
-$config['googleplus']['application_name'] = 'plusttestrajesh';
-$config['googleplus']['client_id'] = '531752385027-mv3ctm6t8v3for6kcsthtoqa696d8r60.apps.googleusercontent.com';
-$config['googleplus']['client_secret'] = '4xH4G6nn_UMxih9hSKrCWi9B';
-$config['googleplus']['redirect_uri'] = 'http://googlecodeigniter.com/';
-$config['googleplus']['api_key'] = 'AIzaSyA7pm6Tqfl5M9eM1_V1KpbEDJB9FF4gAi0';
+$config['googleplus']['application_name'] = ''; #Use must have to use same application name which you register with google. Using APIs & Auth->Consent Screen
+$config['googleplus']['client_id'] = '';
+$config['googleplus']['client_secret'] = '';
+$config['googleplus']['redirect_uri'] = ''; #Add redirect url which you add in google console.
+$config['googleplus']['api_key'] = ''; #Add Browser Key
 $config['googleplus']['scopes'] = Array('https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/plus.login','https://www.googleapis.com/auth/plus.moments.write');
 $config['googleplus']['actions'] = Array('http://schemas.google.com/AddActivity');
 ?>
